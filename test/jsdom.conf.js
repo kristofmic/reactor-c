bafb0cf12ee653ded6d54b3d62cf19403bd20c29
Chris Hourihan <chourihan@pindropsecurity.com>
2017-05-10 14:17:13 -0400

    dep update

--- test/jsdom.conf.js
+++ test/jsdom.conf.js
@@ -3,15 +3,16 @@ JSDOM (or more accurately a full DOM API) is required to do
 full rendering with Enzyme (http://airbnb.io/enzyme/docs/api/mount.html)
 */
 
-const jsdom = require('jsdom').jsdom;
+const JSDOM = require('jsdom').JSDOM;
 
 // Setup global variables like we (or other libraries) might expect in the browser
 // This includes document, window, navigator, and all properties on window that are not undefined
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
+const dom = new JSDOM('');
+global.window = dom.window;
+global.document = dom.window.document;
+Object.keys(window).forEach((property) => {
   if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property];
+    global[property] = window[property];
   }
 });
 global.navigator = {
